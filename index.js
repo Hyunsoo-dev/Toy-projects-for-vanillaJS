444ee363ae1962074d317193cb5240759caa3f15
Hyunsoo-dev <jing07161@gmail.com>
2021-06-04 00:27:24 +0900

    Code refactoring

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-const input = document.querySelector("input[type='text']");
-const checkButton = document.querySelector("#checkButton");
-const itemContainer = document.querySelector(".itemContainer");
-const trashIcon = document.querySelector(".fas.fa-trash-alt");
-const completeItem = (event) => {
-  let toBeCompleteItem = event.target;
-  let optionElement = toBeCompleteItem.parentNode;
-  let itemElement = optionElement.parentNode;
-  let itemContainer = itemElement.parentNode;
-  itemContainer.removeChild(itemElement);
-};
-const removeItem = (event) => {
-  let toBeRemoveItem = event.target;
-  let optionElement = toBeRemoveItem.parentNode;
-  let itemElement = optionElement.parentNode;
-  let itemContainer = itemElement.parentNode;
-  itemContainer.removeChild(itemElement);
-};
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
 
-const addItem = () => {
-  let itemValue = input.value;
-
-  let item = document.createElement("div");
-  let name = document.createElement("div");
-  let option = document.createElement("div");
-  let trashIcon = document.createElement("i");
-  let checkIcon = document.createElement("i");
-
-  item.setAttribute("class", "item");
-  name.setAttribute("class", "name");
-  option.setAttribute("class", "option");
-  trashIcon.setAttribute("class", "fas fa-trash-alt");
-  checkIcon.setAttribute("class", "fas fa-check");
-
-  name.textContent = itemValue;
-  option.append(checkIcon, trashIcon);
-  item.append(name, option);
-  itemContainer.append(item);
-  input.value = "";
-
-  trashIcon.addEventListener("click", removeItem);
-  checkIcon.addEventListener("click", completeItem);
-};
-const pressEnter = (event) => {
-  if (event.code === "Enter") {
-    event.preventDefault();
-    addItem();
+function onAdd() {
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
   }
-};
-checkButton.addEventListener("click", addItem);
-input.addEventListener("keypress", pressEnter);
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView();
+  input.value = "";
+  input.focus();
+}
 
-// trashIcon.addEventListener("click", removeItem);
+function createItem(text) {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const name = document.createElement("span");
+  name.setAttribute("class", "item__name");
+  name.textContent = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", "item__delete");
+  deleteBtn.innerHTML = "<i class='fas fa-trash-alt'></i>";
+  deleteBtn.addEventListener("click", () => {
+    items.removeChild(itemRow);
+  });
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class", "item__divider");
+
+  item.append(name, deleteBtn);
+  itemRow.append(item, itemDivider);
+  return itemRow;
+}
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    onAdd();
+  }
+});
